87e238937937a40cf812efdde9be760ef66cde96
staccato <moveq@riseup.net>
2021-08-29 18:56:39 +0100

    platform agnostic

--- flake.nix
+++ flake.nix
@@ -2,20 +2,25 @@
   description = "MCPE Launcher for Linux";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   };
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux = 
-      with import nixpkgs { system = "x86_64-linux"; };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}; 
+        arch = nixpkgs.lib.removeSuffix "-linux" "${system}";
+      in rec {
+        
+        defaultPackage = with pkgs; appimageTools.wrapType2 {
+          name = "mcpe-launcher";
 
-      appimageTools.wrapType2 {
-        name = "mcpe-launcher";
-
-        src = fetchurl {
-          url = "https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/v0.2.5-672/Minecraft_Bedrock_Launcher-x86_64-0.0.672.AppImage";
-          sha256 = "sha256-qGWi0diAPYJoxnyzAwkOm1Ojki26BGgvYX1cBO+ji6g=";
+          src = fetchurl {
+            url = "https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/v0.2.5-672/Minecraft_Bedrock_Launcher-${arch}-0.0.672.AppImage";
+            sha256 = "sha256-qGWi0diAPYJoxnyzAwkOm1Ojki26BGgvYX1cBO+ji6g=";
+          };
         };
-      };
-  };
+      }
+    );
 }
