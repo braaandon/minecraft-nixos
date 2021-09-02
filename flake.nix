db8ed4af2e8a2c9f75e3eda0de20e04a09d8c6b8
staccato <moveq@riseup.net>
2021-09-02 20:48:54 +0100

    github editor

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 
           src = fetchurl {
             url = "https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/v0.2.1-hook-2-663/Minecraft_Bedrock_Launcher-${arch}-0.0.663.AppImage";
-            sha256 = "sha256-9TkGMm8BjKpJJ9s5/oT4KT7WtU3ypkVr9QIsQnQMDe4=";
+            sha256 = "sha256-qxx4xzRFnfkjBQmLENmPYx/vcQIDaFnZRdkR48VP04U=";
           };
         };
       }
